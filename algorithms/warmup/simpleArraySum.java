4922fb7e5551ea30304f62bef2025cc4d53d7580
Neota Moe <neota.moe@gmail.com>
2018-02-13 19:35:20 -0600

    algorithms first few problems

---
+++ algorithms/warmup/simpleArraySum.java
@@ -0,0 +1,26 @@
+// Given an array of n integers, print the sum of the array's elements as a single integer.
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int simpleArraySum(int n, int[] ar) {
+        // Complete this function
+        return Arrays.stream(ar).sum();
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        int result = simpleArraySum(n, ar);
+        System.out.println(result);
+    }
+}
