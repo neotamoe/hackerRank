e5a41052cfdf70c3e6550322c04a5e456880fd6c
Neota Moe <neota.moe@gmail.com>
2018-03-29 17:44:42 -0500

    division

---
+++ python/introduction/division.py
@@ -0,0 +1,6 @@
+python division: The first line should contain integer division, a // b . The second line should contain float division, a / b.
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
