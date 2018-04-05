152d24db37b5205094f2ed0588a72ab434b63445
Neota Moe <neota.moe@gmail.com>
2018-04-04 20:22:00 -0500

    big math with integers

---
+++ python/math/bigIntegers.py
@@ -0,0 +1,13 @@
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print(a**b + c**d)
+
+
+# Integers in Python can be as big as the bytes in your machine's memory.
+# There is no limit in size as there is: 2^31 -1 (c++ int) or 2^63 -1 (C++ long long int).
+#
+# As we know, the result of a^b grows really fast with increasing b.
+# Print the result of a^b + c^d on one line.
