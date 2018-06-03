62fc005884925aea61110f0993051b1ee14b93cd
Neota Moe <neota.moe@gmail.com>
2018-06-03 11:35:50 -0500

    easy sunday string exercise

---
+++ strings/camelCase.java
@@ -0,0 +1,45 @@
+// Alice wrote a sequence of words in CamelCase as a string of letters, s, having the following properties:
+// 
+// It is a concatenation of one or more words consisting of English letters.
+// All letters in the first word are lowercase.
+// For each of the subsequent words, the first letter is uppercase and rest of the letters are lowercase.
+// Given s, print the number of words in s on a new line.
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the camelcase function below.
+    static int camelcase(String s) {
+        int wordCount = 1;
+        for(int i=0; i<s.length(); i++){
+            if(Character.isUpperCase(s.charAt(i))){
+                wordCount++;
+            }
+        }
+        return wordCount;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        int result = camelcase(s);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
