ebd4375e8b9b0489a0f65e4a445352ad0e9a1dda
Neota Moe <neota.moe@gmail.com>
2018-07-13 06:34:40 -0500

    using [^] to exclude specific chars

--- regex/excludingSpecificChars.java
+++ regex/excludingSpecificChars.java
@@ -1,17 +1,17 @@
-The negated character class [^] matches any character that is not in the square brackets.
-
-Task
-
-You have a test string S.
-Your task is to write a regex that will match S with the following conditions:
-
-S must be of length 6.
-First character should not be a digit ( 0,1,2,3,4,5,6,7,8,9 or 0 ).
-Second character should not be a lowercase vowel ( a,e,i,o or u ).
-Third character should not be b, c, D or F.
-Fourth character should not be a whitespace character ( \r, \n, \t, \f or <space> ).
-Fifth character should not be a uppercase vowel ( A,E,I,O or U ).
-Sixth character should not be a . or , symbol.
+// The negated character class [^] matches any character that is not in the square brackets.
+//
+// Task
+//
+// You have a test string S.
+// Your task is to write a regex that will match S with the following conditions:
+// 
+// S must be of length 6.
+// First character should not be a digit ( 0,1,2,3,4,5,6,7,8,9 or 0 ).
+// Second character should not be a lowercase vowel ( a,e,i,o or u ).
+// Third character should not be b, c, D or F.
+// Fourth character should not be a whitespace character ( \r, \n, \t, \f or <space> ).
+// Fifth character should not be a uppercase vowel ( A,E,I,O or U ).
+// Sixth character should not be a . or , symbol.
 
 import java.io.*;
 import java.util.*;
