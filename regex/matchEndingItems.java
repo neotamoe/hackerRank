910b79d14b15eaae57fd8202f29b1165e9cfc8f8
Neota Moe <neota.moe@gmail.com>
2018-07-14 08:41:40 -0500

    using $ to match at end of string

---
+++ regex/matchEndingItems.java
@@ -0,0 +1,37 @@
+// The $ boundary matcher matches an occurrence of a character/character class/group at the end of a line.
+//
+// Task
+// Write a RegEx to match a test string, S, under the following conditions:
+//
+// S should consist of only lowercase and uppercase letters (no numbers or symbols).
+// S should end in s.
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+        Regex_Test tester = new Regex_Test();
+        tester.checker("^[a-zA-Z]*s$"); // Use \\ instead of using \
+
+    }
+}
+
+
+class Regex_Test {
+
+    public void checker(String Regex_Pattern){
+
+        Scanner Input = new Scanner(System.in);
+        String Test_String = Input.nextLine();
+        Pattern p = Pattern.compile(Regex_Pattern);
+        Matcher m = p.matcher(Test_String);
+        System.out.println(m.find());
+    }
+
+}
