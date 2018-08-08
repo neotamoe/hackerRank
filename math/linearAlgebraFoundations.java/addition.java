c6225da71d79d9fb2fe2bd5f7b978c7ad44c09e0
Neota Moe <neota.moe@gmail.com>
2018-08-07 21:02:43 -0500

    matrix addition

---
+++ math/linearAlgebraFoundations.java/addition.java
@@ -0,0 +1,16 @@
+// Add the two  matrices given below and find the integers corresponding to a,b,c,d,e,f,g,h,i and :
+//
+// [1  2  3]       [4  5  6]     [a b c]
+// [2  3  4]   +   [7  8  9]  =  [d e f]
+// [1  1  1]       [4  5  7]     [g h i]
+// To submit your answer, enter the resultant values of each of the nine integers on a new line and click Submit Code. (edited)
+
+5
+7
+9
+9
+11
+13
+5
+6
+8
