b90350bb2ab4d331b469c0d710ad2c38682c249d
Neota Moe <neota.moe@gmail.com>
2018-08-07 21:05:21 -0500

    matrix multiplication with 3x3

---
+++ math/linearAlgebraFoundations.java/3x3matrixMultiplication.java
@@ -0,0 +1,16 @@
+// Two matrices of size 3x3 are multiplied as shown below. Find the integers a, b, c, d, e, f, g, h, i in the resultant matrix.
+//
+// [1  2  3 ]        [4  5  6 ]      [A  B  C ]
+// [2  3  4 ]   x    [7  8  9 ]   =  [D  E  F ] 
+// [1  1  1 ]        [4  5  7 ]      [G  H  I ]
+// In the text box below, enter each of the 9 integers on a new line.
+
+30
+36
+45
+45
+54
+67
+15
+18
+22
