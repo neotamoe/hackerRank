32b86c90aff9e3b03cf0ace768ea86a878ad19a3
Neota Moe <neota.moe@gmail.com>
2018-08-07 21:04:27 -0500

    2x2 matrix multiplication

---
+++ math/linearAlgebraFoundations.java/multiplication2x2.java
@@ -0,0 +1,10 @@
+// Two matrices of size 2x2 are multiplied as shown below. Find the integers A, B, C, D in the resultant matrix.
+//
+// [1  2 ]   x     [4  5 ]      [A  B]
+// [2  3 ]         [7  8 ]   =  [C  D]
+// In the text box below, enter each of the four integers on a new line.
+
+18
+21
+29
+34
